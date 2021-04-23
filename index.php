f2f8412711bc5204c5287645740b14d1fda78df3
Oscar Cano <oscaralfredoca@gmail.com>
2021-04-23 16:50:40 -0500

    Actualizacion

--- index.php
+++ index.php
@@ -45,16 +45,16 @@
                             </h2>
                             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                 <div class="list-group">
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-person-check-fill"></i>&nbsp;&nbsp;Usuario</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-grid-fill"></i>&nbsp;&nbsp;Roles</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-person-lines-fill"></i>&nbsp;&nbsp;Cliente</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-diagram-3-fill"></i></i>&nbsp;&nbsp;Dependencias</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-person-badge"></i>&nbsp;&nbsp;Conductores</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-truck-flatbed"></i>&nbsp;&nbsp;Vehiculos</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-pin-angle-fill"></i>&nbsp;&nbsp;Ciudades</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-signpost-split-fill"></i>&nbsp;&nbsp;Departamento</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-signpost-2-fill"></i>&nbsp;&nbsp;Rutas</a>
-                                <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-grid-1x2-fill"></i>&nbsp;&nbsp;Sucursal</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-person-check-fill"></i>&nbsp;&nbsp;Usuario</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-grid-fill"></i>&nbsp;&nbsp;Roles</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-person-lines-fill"></i>&nbsp;&nbsp;Cliente</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-diagram-3-fill"></i></i>&nbsp;&nbsp;Dependencias</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-person-badge"></i>&nbsp;&nbsp;Conductores</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-truck-flatbed"></i>&nbsp;&nbsp;Vehiculos</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-pin-angle-fill"></i>&nbsp;&nbsp;Ciudades</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-signpost-split-fill"></i>&nbsp;&nbsp;Departamento</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-signpost-2-fill"></i>&nbsp;&nbsp;Rutas</a>
+                                    <a href="#" class="list-group-item list-group-item-action"><i class="bi bi-grid-1x2-fill"></i>&nbsp;&nbsp;Sucursal</a>
 
                                 </div>
                             </div>
@@ -62,12 +62,12 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="flush-headingTwo">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                <i class="bi bi-gear"></i>&nbsp;&nbsp;Operativo
+                                    <i class="bi bi-gear"></i>&nbsp;&nbsp;Operativo
                                 </button>
                             </h2>
                             <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                                 <div class="list-group">
-                                <a href="#" class="list-group-item list-group-item-action">Gestion de Solicitudes</a>
+                                    <a href="#" class="list-group-item list-group-item-action">Gestion de Solicitudes</a>
 
                                 </div>
                             </div>
@@ -75,7 +75,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="flush-headingThree">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                <i class="bi bi-person"></i>&nbsp;&nbsp; Cliente
+                                    <i class="bi bi-person"></i>&nbsp;&nbsp; Cliente
                                 </button>
                             </h2>
                             <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
@@ -85,7 +85,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="flush-headingThree">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                <i class="bi bi-cash-stack"></i>&nbsp;&nbsp; Facturacion
+                                    <i class="bi bi-cash-stack"></i>&nbsp;&nbsp; Facturacion
                                 </button>
                             </h2>
                             <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
@@ -94,7 +94,47 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-10"></div>
+                <div class="col-10">
+                    <section id="informativo">
+                        <div class="container-fluid hv-50 p-2 ">
+                            <div class="row vh-50">
+                                <div class="col-4">
+                                    <div class="accordion">
+                                        <div class="accordion-item bg-danger">
+                                            <h2 class="accordion-header bg-danger" id="headingOne">
+                                                <button class="accordion-button bg-danger text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                    Solicitudes Diarias Pendientes
+                                                </button>
+                                            </h2>
+                                            <div id="collapseOne" class="accordion-collapse collapse show text-light" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <strong>This is the first item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="headingTwo">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                Accordion Item #2
+                                            </button>
+                                        </h2>
+                                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                            <div class="accordion-body">
+                                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                    </section>
+                </div>
             </div>
         </div>
     </main>
