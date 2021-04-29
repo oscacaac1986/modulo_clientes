6b9a6789e8e7027b41b155afc30942dad8ff28e1
Oscar Cano <oscaralfredoca@gmail.com>
2021-04-29 08:43:48 -0500

    Actualizacion Dashboard

--- index.php
+++ index.php
@@ -96,19 +96,19 @@
                 </div>
                 <div class="col-10">
                     <section id="informativo">
-                        <div class="container-fluid hv-50 p-2 ">
+                        <div class="container-fluid hv-50 p-1 ">
                             <div class="row vh-50">
                                 <div class="col-4">
                                     <div class="accordion">
-                                        <div class="accordion-item bg-danger">
+                                        <div class="accordion-item ">
                                             <h2 class="accordion-header bg-danger" id="headingOne">
                                                 <button class="accordion-button bg-danger text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                                     Solicitudes Diarias Pendientes
                                                 </button>
                                             </h2>
-                                            <div id="collapseOne" class="accordion-collapse collapse show text-light" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                                                <div class="accordion-body">
-                                                    <strong>This is the first item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                            <div id="collapseOne" class="accordion-collapse collapse show border border-danger" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body text-center" style="font-size: 30px;">
+                                                    <a href="http://" target="_blank" rel="noopener noreferrer"><strong>30</strong></a>
                                                 </div>
                                             </div>
                                         </div>
@@ -117,22 +117,72 @@
                                 <div class="col-4">
                                     <div class="accordion-item">
                                         <h2 class="accordion-header" id="headingTwo">
-                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                Accordion Item #2
+                                            <button class="accordion-button collapsed bg-success text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                Solicitudes Diarias en Proceso
                                             </button>
                                         </h2>
-                                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                            <div class="accordion-body">
-                                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                        <div id="collapseTwo" class="accordion-collapse collapse show border border-success" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                            <div class="accordion-body text-center" style="font-size: 30px;">
+                                                <a href="http://" target="_blank" rel="noopener noreferrer"><strong>50</strong></a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-4">
-                                        
+                                </div>
+                                <div class="col-4">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="headingThree">
+                                            <button class="accordion-button collapsed bg-warning text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                Solicitudes Diarias Terminadas
+                                            </button>
+                                        </h2>
+                                        <div id="collapseThree" class="accordion-collapse collapse show border border-warning" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                            <div class="accordion-body text-center" style="font-size: 30px;">
+                                                <a href="http://" target="_blank" rel="noopener noreferrer"><strong>50</strong></a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
 
+                    </section>
+                    <section>
+                        <div class="container-fluid vh-20 p-1">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="accordion">
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingFour">
+                                                <button type="button" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                                                    Consolidado de Solicitudes por mes.
+                                                </button>
+                                            </h2>
+                                            <div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                                                <div class="according-body">
+                                                    <canvas id="grafic_pedidos_mes"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="accordion">
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingFive">
+                                                <button type="button" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                                                    Consolidado de Solicitudes por mes.
+                                                </button>
+                                            </h2>
+                                            <div id="collapseFive" class="accordion-collapse collapse show" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                                                <div class="according-body">
+                                                    <canvas id="grafic_pedidos_mesdos"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </section>
                 </div>
             </div>
@@ -149,7 +199,10 @@
     </footer>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
+    <!-- char.js Library for grafic stadistic -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!-- archivo_graficas  -->
+    <script src="graficas.js"></script>
 </body>
 
 </html>
